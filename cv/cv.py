4cad42f6e6cd4c415617a4ea67e4130db35dea60
Vince Knight <vincent.knight@gmail.com>
2017-05-18 21:50:10 +0100

    Rewritten CV.

--- cv/cv.py
+++ cv/cv.py
@@ -3,83 +3,61 @@ from latex.jinja2 import make_env
 from latex import build_pdf
 import yaml
 import os
+import glob
 
-# Read appointments
-with open("../_data/appointments.yml", "r") as f:
-    appointments = yaml.load(f)
+data_dir = "../data/"
 
-# Read qualifications
-with open("../_data/qualifications.yml", "r") as f:
-    qualifications = yaml.load(f)
+def read_dir(directory):
+    """
+    Read all yaml files in a directory
+    """
+    dictionaries = []
+    for f in glob.glob("{}/*yml".format(directory)):
+        with open(f, "r") as f:
+            dictionaries.append(yaml.load(f))
+    return dictionaries
 
-# Read publications
-with open("../_data/publications.yml", "r") as f:
-    publications = yaml.load(f)
+kwargs = {}
+for var, directory in (("appointments", "Appointment"),
+                       ("qualifications", "Qualification"),
+                       ("publications", "Publication"),
+                       ("students", "StudentProject"),
+                       ("courses", "Course"),
+                       ("grants", "Funding"),
+                       ("outreach_activities", "Outreach"),
+                       ("software_projects", "SoftwareProject"),
+                       ("software_communities", "SoftwareCommunity"),
+                       ("roles", "Role"),
+                       ("media", "Media"),
+                       ("interests", "ResearchTopic"),
+                       ("awards", "Award")):
+    kwargs[var] = read_dir("{}{}".format(data_dir, directory))
 
-# Read students
-with open("../_data/students.yml", "r") as f:
-    students = yaml.load(f)
+collaborators = {}
+for yaml_file in glob.glob("{}Collaborator/*yml".format(data_dir)):
+    with open(yaml_file, "r") as f:
+        name = yaml.load(f)["name"]
+        collaborators[yaml_file[len(data_dir + "Collaborator/"):-len(".yml")]] = name
 
-# Read courses
-with open("../_data/courses.yml", "r") as f:
-    courses = yaml.load(f)
 
-# Read grants
-with open("../_data/grants.yml", "r") as f:
-    grants = yaml.load(f)
+for publication in kwargs["publications"]:
+    for pos, author in enumerate(publication["authors"]):
+        publication["authors"][pos] = collaborators[author]
 
-# Read media
-with open("../_data/media.yml", "r") as f:
-    media = yaml.load(f)
+for student in kwargs["students"]:
+    student["name"] = collaborators[student["student"]]
 
-# Read outreach
-with open("../_data/outreach_activities.yml", "r") as f:
-    outreach_activities = yaml.load(f)
-
-# Read software projects
-with open("../_data/software_projects.yml", "r") as f:
-    software_projects = yaml.load(f)
-
-# Read software communities
-with open("../_data/software_communities.yml", "r") as f:
-    software_communities = yaml.load(f)
-
-# Read roles
-with open("../_data/roles.yml", "r") as f:
-    roles = yaml.load(f)
-
-# Read research interests
-with open("../_data/research_topics.yml", "r") as f:
-    interests = yaml.load(f)
-
-# Read awards
-with open("../_data/awards.yml", "r") as f:
-    awards = yaml.load(f)
-
+kwargs["publications"].sort(key=lambda x: x["date"])
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 env = make_env(loader=FileSystemLoader('.'))
 tpl = env.get_template('cv.latex')
 
-tex = tpl.render(appointments=appointments,
-                 qualifications=qualifications,
-                 publications=publications,
-                 students=students,
-                 courses=courses,
-                 grants=grants,
-                 media=media,
-                 outreach_activities=outreach_activities,
-                 software_projects=software_projects,
-                 software_communities=software_communities,
-                 roles=roles,
-                 interests=interests,
-                 awards=awards,
-                 )
-
+tex = tpl.render(**kwargs)
 # Write tex
 with open("vince-knight.tex", 'w') as f:
-    f.write(tex)
+   f.write(tex)
 
 # Write pdf
 pdf = build_pdf(tex, texinputs=[current_dir, ''])
